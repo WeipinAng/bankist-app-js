43f2f628e7be2d7a5c76ec2a6c4308548e7b6503
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-09-02 16:11:19 +0800

    implement feature to request a loan from the bank (certain conditions apply) using some() and update UI accordingly

--- script.js
+++ script.js
@@ -161,6 +161,20 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  // only grants a loan if there is any deposit with at least 10% of requested loan amount
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 

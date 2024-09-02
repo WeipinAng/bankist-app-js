45bae3bdd2361af2ef681a28cd2822bed51d81e7
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-09-02 14:49:25 +0800

    log user out (hide UI) after deleting user account successfully

--- script.js
+++ script.js
@@ -172,6 +172,8 @@ btnClose.addEventListener('click', function (e) {
       acc => acc.username === currentAccount.username
     );
     accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
   }
 
   inputCloseUsername.value = inputClosePin.value = '';
